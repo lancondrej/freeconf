9fd663df3899427e7bfe1f4a13bb28447a853272
Ondřej Lanč <lancondr@fjfi.cvut.cz>
2016-11-09 12:20:51 +0100

    template i multiple

--- src/IO/XMLParser/template_file.py
+++ src/IO/XMLParser/template_file.py
@@ -6,6 +6,8 @@ from src.Model.Config.group import Group
 from src.Model.Package.entries.bool import Bool
 from src.Model.Package.entries.container import Container
 from src.Model.Package.entries.fuzzy import Fuzzy
+from src.Model.Package.entries.multiple.multiple_container import MultipleContainer
+from src.Model.Package.entries.multiple.multiple_key_word import MultipleKeyWord
 from src.Model.Package.entries.number import Number
 from src.Model.Package.entries.string import String
 
@@ -13,10 +15,11 @@ __author__ = 'Ondřej Lanč'
 
 
 class EntryType(object):
-    def __init__(self, name, name_element, class_name, function=None):
+    def __init__(self, name, name_element, class_name, multiple,function=None):
         self.name = name
         self.name_element = name_element
         self.class_name = class_name
+        self.multiple_class = multiple
         self.inside_func = function
 
 
@@ -29,17 +32,14 @@ class TemplateFileReader(FileReader):
         super().__init__(template_file)
 
     def parse(self):
-        self._package.tree = self._parse_container(self._root)
-
-    def _parse_container(self, container_element):
-        name = container_element.findtext('container-name')
+        name = self._root.findtext('container-name')
         if name:
             container = Container(name)
-            for entry in self._parse_entry(container_element):
+            for entry in self._parse_entry(self._root):
                 container.add_entry(entry)
-            return container
+            self._package.tree = container
         else:
-            raise ParseError('container name missing')
+            raise ParseError('root container name missing')
 
     def _parse_entry(self, container_element):
         for element_type in self.types:
@@ -52,17 +52,31 @@ class TemplateFileReader(FileReader):
             name = element.findtext(element_type.name_element)
             if name:
                 entry = element_type.class_name(name)
+                # Multiple manipulation
+                multiple = element.find('multiple')
+                if multiple:
+                    entry = element_type.multiple_class(entry)
+                    value = multiple.findtext('max')
+                    if value is not None:
+                        entry.multiple_max = int(value)
+                    value = multiple.findtext('min')
+                    if value is not None:
+                        entry.multiple_min = int(value)
+                    entry.primary = multiple.findtext('primary')
+                # BaseEntry properties
+                # active
                 value = element.findtext('active')
                 if value is not None:
                     entry.static_active = True if value == 'yes' else False
-
+                # mandatory
                 value = element.findtext('mandatory')
                 if value is not None:
                     entry.static_mandatory = True if value == 'yes' else False
-
+                # group
                 value = element.findtext('group')
                 if value is not None:
                     entry.group = value
+                # other properties for specific type, and container recursion
                 if element_type.inside_func:
                     element_type.inside_func(self, entry, element)
                 yield entry
@@ -90,10 +104,10 @@ class TemplateFileReader(FileReader):
             value=properties.findtext('precision')
             if value is not None:
                 entry.precision = float(value)
-            value=properties.findtext('sign')
+            value=properties.findtext('print-sign')
             if value is not None:
                 entry.print_sign = True if value == 'yes' else False
-            value=properties.findtext('zeros')
+            value=properties.findtext('leading-zeros')
             if value is not None:
                 entry.leading_zeros = True if value == 'yes' else False
 
@@ -103,12 +117,17 @@ class TemplateFileReader(FileReader):
             value=properties.findtext('regexp')
             if value is not None:
                 entry.reg_exp=value
+            # TODO: <data>
+
+    def _inside_fuzzy(self, entry, element):
+        pass
+        # TODO: <data>
 
     types = [
-        EntryType('container', 'container-name', Container, _inside_container),
-        EntryType('bool', 'entry-name', Bool),
-        EntryType('string', 'entry-name', String, _inside_string),
-        EntryType('fuzzy', 'entry-name', Fuzzy),
-        EntryType('number', 'entry-name', Number, _inside_number)
+        EntryType('container', 'container-name', Container, MultipleContainer, _inside_container),
+        EntryType('bool', 'entry-name', Bool, MultipleKeyWord),
+        EntryType('string', 'entry-name', String, MultipleKeyWord, _inside_string),
+        EntryType('fuzzy', 'entry-name', Fuzzy, MultipleKeyWord, _inside_fuzzy),
+        EntryType('number', 'entry-name', Number, MultipleKeyWord, _inside_number)
     ]
 
